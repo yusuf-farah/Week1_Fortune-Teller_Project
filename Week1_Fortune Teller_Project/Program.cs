e14e35e8a5336554634826cac9ec842aa01f010b
yusuf-farah <yfarah1577@gmail.com>
2017-09-29 15:27:57 -0400

    finish part one of the project

--- Week1_Fortune Teller_Project/Program.cs
+++ Week1_Fortune Teller_Project/Program.cs
@@ -12,7 +12,29 @@ namespace Week1_Fortune_Teller_Project
         {
             // project part 1
             Console.WriteLine("what is your first name");
+            string firstName = Console.ReadLine();
             Console.WriteLine("what is you last name");
+            string lastNme = Console.ReadLine();
+            Console.WriteLine("how old are you");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("what month where you born");
+            int brMonth = int.Parse(Console.ReadLine());
+            Console.WriteLine("what is your favorite ROYGBIV color if you don,t know what ROYGBIV is enter help");
+            string ROYGBIV = Console.ReadLine();
+            
+
+            
+             {
+                
+                    }
+
+            
+            
+
+
+
+
+
         }
     }
 }
