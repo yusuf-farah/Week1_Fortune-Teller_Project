3f49039d30238ac91cba471b6aea18d6bfb03e44
yusuf-farah <yfarah1577@gmail.com>
2017-10-01 16:50:46 -0400

    I did what i can

--- Week1_Fortune Teller_Project/Program.cs
+++ Week1_Fortune Teller_Project/Program.cs
@@ -24,10 +24,21 @@ namespace Week1_Fortune_Teller_Project
 
             Console.WriteLine("in a number format what month where you born");
             int brMonth = int.Parse(Console.ReadLine());
+            int janMonth = 1;
+            
 
             Console.WriteLine("what is your favorite ROYGBIV color if you don,t know what ROYGBIV is enter help");
             string ROYGBIV = Console.ReadLine().ToLower();
-            if(ROYGBIV == "help")
+            string Red = "red";
+            string orange = "orange";
+            string Yellow = "yellow";
+            String Green = "green";
+            string Blue = "Blue";
+            String Indigo = "Indigo";
+            string Violet = "violet";
+
+
+            if (ROYGBIV == "help")
             {
                 Console.WriteLine("The colors are, Red, Orange, Yellow, Green, Blue, Indigo, and Violet");
             }
@@ -38,37 +49,128 @@ namespace Week1_Fortune_Teller_Project
 
             Console.Write(" how many siblings do you have?");
             int Siblings = int.Parse(Console.ReadLine());
+            int zeroSiblings = 0;
+            int oneSiblings = 1;
+            int twoSiblings = 2;
+            int threeSiblings = 3;
+
+            
+
             //part 2
-           
-            if (age  % 2==1)
+
+            Console.WriteLine("you fortune will be display momenterly");
+
+            Console.WriteLine(firstName + " " + lastNme); 
+            if(age %2==0)
             {
-                Console.WriteLine(" congratualtions!! you will retire at age of 45 years");
+                Console.WriteLine("you will retire at the  age of 50");
             }
         else
             {
-                Console.WriteLine("you will retire in at the age of  50");
+                Console.WriteLine("you will retire at the age of 45");
             }
 
-            if (Siblings == 0)
+
+            if ( brMonth== janMonth)
             {
-                Console.WriteLine(" awsome  your vacation home will be  in florida");
+                Console.WriteLine("you will have 2m dollar your bank by the time of your retirement age");
             }
+            else if (brMonth == 5 - 8)
+            {
+                Console.WriteLine(" congrat you will have 200k in your bank at the time of retirement");
+            }
+            else if (brMonth == 9 - 12)
+            {
+                Console.WriteLine("you will 100k in your bank at the time of retirement ");
+            }
+            else
+            {
+                Console.WriteLine("oops! you will have zero dollar in your bank");
 
-            
+                if (Siblings == zeroSiblings)
+                {
+                    Console.WriteLine("you will have vacation home in the bahamas");
+                }
+                else if (Siblings == oneSiblings)
+                {
+                    Console.WriteLine("you will have vacation in Mugadishu");
+                }
+                else if (Siblings == twoSiblings)
+                {
+                    Console.WriteLine("you will have vacation home in Zinzibar");
+                }
+                else if (Siblings == threeSiblings)
+                {
+                    Console.WriteLine("you will have vacation home in Malta");
+                }
+                else
+                {
+                    Console.WriteLine(" you will have vacation in cleveland");
+                }
 
-             
-            
+                if (ROYGBIV== Red)
 
-            
-             
-
-            
-            
+                {
+                    Console.WriteLine("congrat you will be driving Liclon MKS");
+                }
 
 
+                if (ROYGBIV == orange)
+
+                {
+                    Console.WriteLine("congrat you will be driving new fully loaded cadilac ");
+                }
+
+
+                if (ROYGBIV == Yellow)
+
+                {
+                    Console.WriteLine("congrat you will be driving tahoe ");
+                }
+
+
+                if (ROYGBIV == Green)
+
+                {
+                    Console.WriteLine("congrat you will be driving mercury ");
+                }
+
+                if (ROYGBIV == Blue)
+
+                {
+                    Console.WriteLine("congrat you will be riding  yatch ");
+                }
+
+                if (ROYGBIV == Indigo)
+
+                {
+                    Console.WriteLine("congrat you will be flying private jicket ");
+                }
+
+                if (ROYGBIV == Violet)
+
+                {
+                    Console.WriteLine("congrat you will be driving luxury lexus  ");
+                }
+                else
+                {
+                    Console.WriteLine(" sorry you will be calling uber");
+                        }
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+            }
         }
     }
 }
